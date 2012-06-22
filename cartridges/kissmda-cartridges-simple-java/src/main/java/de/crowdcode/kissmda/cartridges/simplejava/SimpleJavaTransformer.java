1f292343fe64523fdbd4a18ee6b6513146ece2b8
lofi <lofidewanto@gmail.com>
2012-06-22 21:51:31 +0200

    add ast

--- cartridges/kissmda-cartridges-simple-java/src/main/java/de/crowdcode/kissmda/cartridges/simplejava/SimpleJavaTransformer.java
+++ cartridges/kissmda-cartridges-simple-java/src/main/java/de/crowdcode/kissmda/cartridges/simplejava/SimpleJavaTransformer.java
@@ -24,6 +24,16 @@ import java.util.logging.Logger;
 
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ExpressionStatement;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.PackageDeclaration;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.TypeParameter;
 import org.eclipse.uml2.uml.Element;
 
 import de.crowdcode.kissmda.core.Context;
@@ -67,8 +77,45 @@ public class SimpleJavaTransformer implements Transformer {
 				logger.log(Level.INFO, index + " - " + element.toString());
 				index++;
 			}
+
+			createClazz();
 		} catch (URISyntaxException e) {
 			throw new TransformerException(e);
 		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public void createClazz() {
+		AST ast = AST.newAST(AST.JLS3);
+		CompilationUnit cu = ast.newCompilationUnit();
+
+		PackageDeclaration p1 = ast.newPackageDeclaration();
+		p1.setName(ast.newSimpleName("foo"));
+		cu.setPackage(p1);
+
+		ImportDeclaration id = ast.newImportDeclaration();
+		id.setName(ast.newName(new String[] { "java", "util", "Set" }));
+		cu.imports().add(id);
+
+		TypeDeclaration td = ast.newTypeDeclaration();
+		td.setName(ast.newSimpleName("Foo"));
+		TypeParameter tp = ast.newTypeParameter();
+		tp.setName(ast.newSimpleName("X"));
+		td.typeParameters().add(tp);
+		cu.types().add(td);
+
+		MethodDeclaration md = ast.newMethodDeclaration();
+		td.bodyDeclarations().add(md);
+
+		Block block = ast.newBlock();
+		md.setBody(block);
+
+		MethodInvocation mi = ast.newMethodInvocation();
+		mi.setName(ast.newSimpleName("x"));
+
+		ExpressionStatement e = ast.newExpressionStatement(mi);
+		block.statements().add(e);
+
+		System.out.println(cu);
 	}
 }
