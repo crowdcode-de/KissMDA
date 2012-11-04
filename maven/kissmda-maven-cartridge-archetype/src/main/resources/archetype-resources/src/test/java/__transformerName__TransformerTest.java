31b0b75112423c601c4717e92bbc70dbe4633fc9
lofi <lofidewanto@gmail.com>
2012-11-04 23:17:02 +0100

    update test class

--- maven/kissmda-maven-cartridge-archetype/src/main/resources/archetype-resources/src/test/java/__transformerName__TransformerTest.java
+++ maven/kissmda-maven-cartridge-archetype/src/main/resources/archetype-resources/src/test/java/__transformerName__TransformerTest.java
@@ -18,9 +18,23 @@
  */
 package ${groupId};
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.logging.Logger;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import de.crowdcode.kissmda.core.Context;
+import de.crowdcode.kissmda.core.StandardContext;
+import de.crowdcode.kissmda.core.TransformerException;
+import de.crowdcode.kissmda.core.file.FileWriter;
+import de.crowdcode.kissmda.core.jdt.DataTypeUtils;
+import de.crowdcode.kissmda.core.jdt.JdtHelper;
+import de.crowdcode.kissmda.core.jdt.MethodHelper;
+import de.crowdcode.kissmda.core.uml.PackageHelper;
+import de.crowdcode.kissmda.core.uml.ReaderWriter;
 
 /**
  * Unit test for simple app.
@@ -33,7 +47,7 @@ public class ${transformerName}TransformerTest {
 
 	private static final Logger logger = Logger
 			.getLogger(${transformerName}TransformerTest.class.getName());
-	private SimpleJavaTransformer simpleJavaTransformer;
+	private ${transformerName}Transformer transformer;
 	private Context context;
 	private PackageHelper packageHelper;
 	private MethodHelper methodHelper;
@@ -41,7 +55,6 @@ public class ${transformerName}TransformerTest {
 	private FileWriter fileWriter;
 	private DataTypeUtils dataTypeUtils;
 	private JdtHelper jdtHelper;
-	private InterfaceGenerator interfaceGenerator;
     
     @Before
 	public void setUp() {
@@ -54,14 +67,9 @@ public class ${transformerName}TransformerTest {
 		jdtHelper.setDataTypeUtils(dataTypeUtils);
 		jdtHelper.setPackageHelper(packageHelper);
 		packageHelper.setReaderWriter(readerWriter);
-		interfaceGenerator = new InterfaceGenerator();
-		interfaceGenerator.setJdtHelper(jdtHelper);
-		interfaceGenerator.setMethodHelper(methodHelper);
-		interfaceGenerator.setPackageHelper(packageHelper);
-		simpleJavaTransformer = new SimpleJavaTransformer();
-		simpleJavaTransformer.setPackageHelper(packageHelper);
-		simpleJavaTransformer.setFileWriter(fileWriter);
-		simpleJavaTransformer.setInterfaceGenerator(interfaceGenerator);
+		transformer = new ${transformerName}Transformer();
+		transformer.setPackageHelper(packageHelper);
+		transformer.setFileWriter(fileWriter);
 		context = new StandardContext();
 	}
     
@@ -73,7 +81,7 @@ public class ${transformerName}TransformerTest {
 			logger.info("Path: " + thisPath);
 			context.setSourceModel(thisPath + "model/emf/test-uml.uml");
 			context.setTargetModel("target/generated-sources/java-transformer");
-			simpleJavaTransformer.transform(context);
+			transformer.transform(context);
 		} catch (TransformerException e) {
 			assertFalse(true);
 		}
