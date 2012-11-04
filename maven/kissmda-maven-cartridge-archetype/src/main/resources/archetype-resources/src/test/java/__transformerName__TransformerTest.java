5938668cce1285663c5125792d219c5d98dbb9df
lofi <lofidewanto@gmail.com>
2012-11-04 23:02:02 +0100

    add test class

--- maven/kissmda-maven-cartridge-archetype/src/main/resources/archetype-resources/src/test/java/__transformerName__TransformerTest.java
+++ maven/kissmda-maven-cartridge-archetype/src/main/resources/archetype-resources/src/test/java/__transformerName__TransformerTest.java
@@ -31,11 +31,52 @@ import org.junit.Test;
  */
 public class ${transformerName}TransformerTest {
 
-    /**
-     * Rigourous Test.
-     */
+	private static final Logger logger = Logger
+			.getLogger(${transformerName}TransformerTest.class.getName());
+	private SimpleJavaTransformer simpleJavaTransformer;
+	private Context context;
+	private PackageHelper packageHelper;
+	private MethodHelper methodHelper;
+	private ReaderWriter readerWriter;
+	private FileWriter fileWriter;
+	private DataTypeUtils dataTypeUtils;
+	private JdtHelper jdtHelper;
+	private InterfaceGenerator interfaceGenerator;
+    
+    @Before
+	public void setUp() {
+		readerWriter = new ReaderWriter();
+		packageHelper = new PackageHelper();
+		methodHelper = new MethodHelper();
+		fileWriter = new FileWriter();
+		dataTypeUtils = new DataTypeUtils();
+		jdtHelper = new JdtHelper();
+		jdtHelper.setDataTypeUtils(dataTypeUtils);
+		jdtHelper.setPackageHelper(packageHelper);
+		packageHelper.setReaderWriter(readerWriter);
+		interfaceGenerator = new InterfaceGenerator();
+		interfaceGenerator.setJdtHelper(jdtHelper);
+		interfaceGenerator.setMethodHelper(methodHelper);
+		interfaceGenerator.setPackageHelper(packageHelper);
+		simpleJavaTransformer = new SimpleJavaTransformer();
+		simpleJavaTransformer.setPackageHelper(packageHelper);
+		simpleJavaTransformer.setFileWriter(fileWriter);
+		simpleJavaTransformer.setInterfaceGenerator(interfaceGenerator);
+		context = new StandardContext();
+	}
+    
     @Test
-    public void testApp() {
-      assertTrue(true);
-    }
+	public void testTransform() {
+		try {
+			String thisPath = this.getClass().getProtectionDomain()
+					.getCodeSource().getLocation().getPath();
+			logger.info("Path: " + thisPath);
+			context.setSourceModel(thisPath + "model/emf/test-uml.uml");
+			context.setTargetModel("target/generated-sources/java-transformer");
+			simpleJavaTransformer.transform(context);
+		} catch (TransformerException e) {
+			assertFalse(true);
+		}
+		assertTrue(true);
+	}
 }
