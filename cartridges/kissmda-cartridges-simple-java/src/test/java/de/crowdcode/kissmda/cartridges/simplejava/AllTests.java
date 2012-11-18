7567acb344e5814e94ceb160573b5a5e2cfb0181
idueppe <ingo.dueppe@dueppe.com>
2012-11-18 23:12:36 +0100

    Removed obsolete test.

--- cartridges/kissmda-cartridges-simple-java/src/test/java/de/crowdcode/kissmda/cartridges/simplejava/AllTests.java
+++ cartridges/kissmda-cartridges-simple-java/src/test/java/de/crowdcode/kissmda/cartridges/simplejava/AllTests.java
@@ -30,7 +30,7 @@ import org.junit.runners.Suite.SuiteClasses;
  * @since 1.0.0
  */
 @RunWith(Suite.class)
-@SuiteClasses({ SimpleJavaModuleTest.class, SimpleJavaTransformerTest.class,
+@SuiteClasses({ SimpleJavaModuleTest.class,
 		InterfaceGeneratorTest.class })
 public class AllTests {
 }
